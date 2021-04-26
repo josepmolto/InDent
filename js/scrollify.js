af8f01163f8be673a471f15d5045a2fbfe381c58
Josep Moltó <josepmolto@gmail.com>
2021-04-26 19:53:55 +0200

    disable scrollify on mobile phone

--- js/scrollify.js
+++ js/scrollify.js
@@ -1,6 +1,9 @@
 $(function() {
+  if(Math.min(window.screen.width, window.screen.height) > 768)
+  {
     $.scrollify({
       section : ".section",
       standardScrollElements: ".standard_scroll, .navbar-collapse"
     });
+  }
   });
