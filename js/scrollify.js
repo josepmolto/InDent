e42f4c2d40fe32b4764e890a54fa830c4a685052
Josep Moltó <josepmolto@gmail.com>
2021-06-12 17:04:42 +0200

    try to activate scrollify on mobile phone

--- js/scrollify.js
+++ js/scrollify.js
@@ -1,9 +1,9 @@
 $(function() {
-  if(Math.min(window.screen.width, window.screen.height) > 768)
-  {
+  // if(Math.min(window.screen.width, window.screen.height) > 768)
+  // {
     $.scrollify({
       section : ".section",
       standardScrollElements: ".standard_scroll"
     });
-  }
+  // }
 });
