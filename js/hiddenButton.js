48edc972128d994efc9b2cd3e9a65803eb9642b6
Josep Moltó <josepmolto@gmail.com>
2021-09-05 12:46:22 +0200

    last adjustments

---
+++ js/hiddenButton.js
@@ -0,0 +1,9 @@
+function hideOrShowButton(id) {
+  var element = document.getElementById(id);
+
+  if (element.style.display === "block") {
+    element.style.display = "none";
+  } else {
+    element.style.display = "block";
+  }
+}
